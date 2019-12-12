ecfc7f787341e62de1bfcca9cceaa80dd49b6aa5
Alex R <alexei.rudenko@gmail.com>
2019-12-12 17:43:32 +0100

    First version

---
+++ src/PgQueue.ts
@@ -0,0 +1,113 @@
+import { Pool, PoolClient } from 'pg';
+
+function escSql(name: string) {
+  return '"' + name.replace(/"/g, '""') + '"';
+}
+
+export abstract class PgQueue {
+  private pool: Pool;
+  private queue = 'default';
+  private tableName = '__pg_queue_jobs';
+  private estimatedQueueSize = 0;
+  private lastEstimate = 0;
+  private interval?: NodeJS.Timeout;
+
+  public constructor(connectionString: string, maxConcurrency = 10) {
+    this.pool = new Pool({
+      connectionString,
+      max: maxConcurrency,
+    });
+  }
+
+  public async start() {
+    await this.migrate();
+    this.pool.on('error', err => console.log('error', err));
+    this.interval = setInterval(() => this.process(), 100);
+  }
+
+  public async stop() {
+    if (this.interval) clearInterval(this.interval);
+    await this.pool.end();
+  }
+
+  public async enqueue(data: any) {
+    const client = await this.pool.connect();
+    try {
+      await client.query('BEGIN');
+      await client.query(
+        `INSERT INTO ${escSql(this.tableName)}(queue, data) VALUES ($1, $2)`,
+        [this.queue, JSON.stringify(data)]
+      );
+      await client.query('COMMIT');
+    } catch (e) {
+      await client.query('ROLLBACK');
+      throw e;
+    } finally {
+      client.release();
+    }
+  }
+
+  public abstract async perform(data: any, client: PoolClient): Promise<void>;
+
+  private async process() {
+    while (this.estimatedQueueSize) {
+      this.estimatedQueueSize--;
+      this.dequeue();
+    }
+    this.dequeue();
+  }
+
+  private async dequeue() {
+    const client = await this.pool.connect();
+    try {
+      await client.query('BEGIN');
+      const { rows } = await client.query(
+        `DELETE FROM ${escSql(this.tableName)}
+          WHERE id = (
+            SELECT id
+            FROM ${escSql(this.tableName)}
+            WHERE queue = $1
+            ORDER BY id
+            FOR UPDATE SKIP LOCKED
+            LIMIT 1
+          )
+          RETURNING *;
+        `,
+        [this.queue]
+      );
+      if (rows.length > 0) {
+        await this.perform(rows[0].data, client);
+      }
+      await client.query('COMMIT');
+    } catch (e) {
+      await client.query('ROLLBACK');
+      throw e;
+    } finally {
+      await this.estimateQueueSize(client);
+      client.release();
+    }
+  }
+
+  private async estimateQueueSize(client: PoolClient) {
+    if (+new Date() - this.lastEstimate < 100) {
+      return;
+    }
+    const count = await client.query(
+      `SELECT COUNT(1)
+       FROM ${escSql(this.tableName)}
+       WHERE queue = $1`,
+      [this.queue]
+    );
+    const queueSize = parseInt(count.rows[0].count, 10);
+    if (queueSize > this.estimatedQueueSize) {
+      this.estimatedQueueSize = queueSize;
+    }
+    this.lastEstimate = +new Date();
+  }
+
+  private async migrate() {
+    await this.pool.query(
+      `CREATE TABLE IF NOT EXISTS ${this.tableName}(id bigserial, queue varchar(255), data json)`
+    );
+  }
+}
---
+++ test/PgQueue.test.ts
@@ -0,0 +1,60 @@
+import { PgQueue } from '../src/PgQueue';
+
+function sleep(ms: number) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+describe('PgQueue', () => {
+  it('works', async () => {
+    let numJobs = 1000;
+    let completedJobs = 0;
+    let startQueuing = +new Date();
+    let startProcessing = +new Date();
+    let endQueueing = +new Date();
+    let endProcessing = +new Date();
+
+    class MyWorker extends PgQueue {
+      async perform() {
+        await sleep(300);
+        completedJobs++;
+        if (numJobs === completedJobs) {
+          endProcessing = +new Date();
+        }
+      }
+    }
+
+    const queue = new MyWorker('postgresql://localhost:5434/orkon-private');
+
+    try {
+      const promises = [];
+
+      for (let i = 0; i < numJobs; i++) {
+        promises.push(queue.enqueue({ i }));
+      }
+
+      await promises;
+
+      endQueueing = +new Date();
+
+      startProcessing = +new Date();
+
+      await queue.start();
+
+      while (completedJobs < numJobs) {
+        await sleep(100);
+      }
+    } finally {
+      await queue.stop();
+    }
+
+    const durationQueuing = (endQueueing - startQueuing) / 1000;
+    const durationProcessing = (endProcessing - startProcessing) / 1000;
+
+    console.log(
+      'queueing jobs/s =',
+      numJobs / durationQueuing,
+      'processing jobs/s =',
+      completedJobs / durationProcessing
+    );
+  });
+});
